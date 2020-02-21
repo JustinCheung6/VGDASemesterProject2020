b6c0e233180faeb43b6de6cae2bf131ded4ae562
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2020-02-21 12:46:00 -0800

    Added preliminary Obstacle class
    Very unsure if htis is working or not.

---
+++ DungeonDeliveryService/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private int weightTrigger = 1;
+    Rigidbody2D obstacleCollision;
+    Player deliveryPerson;
+    bool activateObstacle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    } // close Start
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } // close Update
+
+    /**
+     * Function to set activate Obstacle to true.
+     */
+    public void OnCollisionEnter2D(Collision collision)
+    {
+        if(deliveryPerson.getWeight() > weightTrigger)
+        {
+            activateObstacle = true;
+        }
+    } // Close OnCollisionEnter2D
+
+    /**
+     * Function to return the int of the triggerWeight.
+     */
+    public int getWeightToTrigger()
+    {
+        return this.weightTrigger;
+    } // Close getWeightToTrigger
+
+} // close Obstacle
