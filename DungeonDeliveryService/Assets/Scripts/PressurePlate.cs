f8ae6af2466623fb2e2f9863f1e9af9b7f524659
DecimBlade <truongellesia@gmail.com>
2020-03-20 17:59:14 -0700

    Pressureplate script still working on it

--- DungeonDeliveryService/Assets/Scripts/PressurePlate.cs
+++ DungeonDeliveryService/Assets/Scripts/PressurePlate.cs
@@ -4,28 +4,20 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    private void Start()
-    {
-        weight = 100;
-        weight_threshold = 200
-    }
-    
-    [SerializeField]
-    GameObject PressurePlate;
+    [SerializeField] private int weight = 100;
+    [SerializeField] private int weight_threshold = 200;
 
-    bool isOpened = false;
-
-    private void OnTriggerEnter(Collider PressurePlate)
+    [SerializeField] bool isOpened = false;
+    [SerializeField] private BoxCollider2D BC;
+    [SerializeField] private SpriteRenderer SR;
+    private void OnTriggerEnter2D(Collider2D PressurePlate)
     {
-        if (isOpened)
+        
+        if (!isOpened)
         {
             isOpened = true;
-            if (weight)
-            {
-                weight = weight_threshold;
-                PressurePlate.transform.position += new Vector3(10, 0, 0);
-            }
-            
+            BC.enabled = false;
+            SR.enabled = false;
         }
     }
 }
