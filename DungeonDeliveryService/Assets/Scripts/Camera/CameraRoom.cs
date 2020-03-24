6cb77dd0457f24a8878f5dfb5fa1deeec75f4501
JustinCheung6 <justincheung6@gmail.com>
2020-03-24 00:07:42 -0700

    -Added RoomManager script that makes a bunch of compositeColliders for cinemachine camera use -Added DoorManager script that makes a bunch of door (soon to be) colliders. Also for cinemachine camera use Next to do: Animation where camera transitions between rooms

---
+++ DungeonDeliveryService/Assets/Scripts/Camera/CameraRoom.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRoom : MonoBehaviour
+{
+    //Door object that are connected to this room (0 = right wall, 1 = top, 2 = left, 3 = bottom)
+    [SerializeField] private Door[] doors = new Door[4];
+    [SerializeField] private CameraRoom[] rooms = new CameraRoom[4];
+    //Position of room relative to the origin room
+    private int[] gridPosition = null;
+
+    public int x { get => gridPosition[0]; }
+    public int y { get => gridPosition[1]; }
+
+    public void AddDoor(Door door, int index)
+    {
+        if (doors[index] != null)
+            return;
+        doors[index] = door;
+    }
+    public void AddNextRoom(CameraRoom room, int index)
+    {
+        if (rooms[index] != null)
+            return;
+        rooms[index] = room;
+    }
+
+    public int[] IndexRooms(int x, int y, int negX = 0, int negY = 0)
+    {
+        Debug.Log(gridPosition);
+
+        if (gridPosition != null)
+            return new int[] { x, y, negX, negY };
+        gridPosition = new int[] { x, y };
+        
+        int[] thisRoomData = new int[] { x, y, negX, negY };
+
+        for(int i = 0; i < rooms.Length; i++)
+        {
+            Debug.Log(rooms[i]);
+            if (rooms[i] != null)
+            {
+                int[] tempData = thisRoomData;
+                if (i <= 1)
+                    tempData[i]++;
+                else
+                    tempData[i]--;
+
+                int[] nextRoomData = rooms[i].IndexRooms(tempData[0], tempData[1], tempData[2], tempData[3]);
+                if (thisRoomData[0] < nextRoomData[0])
+                    thisRoomData[0] = nextRoomData[0];
+                if (thisRoomData[1] < nextRoomData[1])
+                    thisRoomData[1] = nextRoomData[1];
+                if (thisRoomData[2] > nextRoomData[2])
+                    thisRoomData[2] = nextRoomData[2];
+                if (thisRoomData[3] > nextRoomData[3])
+                    thisRoomData[3] = nextRoomData[3];
+            }
+        }
+
+        return thisRoomData;
+    }
+
+    
+}
---
+++ DungeonDeliveryService/Assets/Scripts/Camera/DoorManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField] private GameObject doorPrefab = null;
+
+    private List<Door> children = null;
+
+    private Tilemap doorTilemap = null;
+
+    private void Awake()
+    {
+        if (doorTilemap == null)
+            doorTilemap = GetComponent<Tilemap>();
+        if (children == null)
+            GetChildren();
+    }
+
+    public List<Door> GetChildren()
+    {
+        if (children != null)
+            return children;
+
+        children = new List<Door>();
+
+        for (int x = doorTilemap.cellBounds.xMin; x < doorTilemap.cellBounds.xMax; x++)
+            for (int y = doorTilemap.cellBounds.yMin; y < doorTilemap.cellBounds.yMax; y++)
+            {
+                Vector3Int cellPos = new Vector3Int(x, y, 0);
+
+                if (doorTilemap.HasTile(cellPos))
+                {
+                    Vector3 doorPosition = doorTilemap.CellToWorld(cellPos) + new Vector3(0.5f,0.5f);
+
+                    GameObject door = Instantiate(doorPrefab, doorPosition, Quaternion.identity, transform);
+                    door.GetComponent<Door>().SetDoor();
+                    children.Add(door.GetComponent<Door>());
+                }
+            }
+        return children;
+    }
+
+}
