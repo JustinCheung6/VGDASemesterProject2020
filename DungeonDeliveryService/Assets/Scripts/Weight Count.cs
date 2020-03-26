16819aa8b2ea855f807e6ae7dfb2d10a8ea743db
DecimBlade <truongellesia@gmail.com>
2020-03-26 03:43:11 -0700

    Added something for the Packages on the HUD. Need to figure out code.

---
+++ DungeonDeliveryService/Assets/Scripts/Weight Count.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WeightCount : MonoBehaviour
+{
+    public Transform player;
+    public Text weightText;
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
