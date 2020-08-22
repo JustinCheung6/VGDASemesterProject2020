b79fb1ef26455ac886102e9fa8bbca54b61a08eb
Steven Duran <stevenduran1@yahoo.com>
2020-08-22 01:43:55 -0700

    Added comments to script and tried to make hole collider go from being active to inactive once the block collided with it

--- DungeonDeliveryService/Assets/HoleObstacle.cs
+++ DungeonDeliveryService/Assets/HoleObstacle.cs
@@ -7,15 +7,42 @@ public class HoleObstacle : MonoBehaviour
 
     public GameObject block;
     public GameObject hole;
+    //public Collider2D holeCol;
+
+    private void Start()
+    {
+        //if (hole.gameObject.CompareTag("Hole"))
+        //{
+            //holeCol.isTrigger = false;
+        //}
+    }
+
+    private void Update()
+    {
+       
+    }
 
     // if block collides with hole, block will fill hole and 
     // the player can proceed 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.CompareTag("Block"))
+
+        if (col.gameObject.CompareTag("Block"))
         {
             //code to be executed on collision
+
+            //if (hole.gameObject.CompareTag("Hole"))
+            //{
+            //holeCol.isTrigger = true;
+            //}
+
+            //holeCol.isTrigger = true;
+
+           // col.isTrigger = true;
+           // hole.GetComponent<Collider2D>().enabled = true;
+
             hole.SetActive(false);
+
             //block.isStatic = true;
             //sets the rigidbody of the block from dynamic to static
             block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
