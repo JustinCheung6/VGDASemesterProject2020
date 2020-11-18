08110315d99d8bde5521481346f29c32b77016df
Britney Khuu <britneykhuu@gmail.com>
2020-11-18 08:42:39 -0800

    Starting platform creation test and script on pittest scene

---
+++ DungeonDeliveryService/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public int distance;
+    public int limLeft;
+    public int limRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlatform();
+    }
+    void movePlatform()
+    {
+        
+    }
+    
+}
