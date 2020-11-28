d3a269895a16c1bd550316fd31ff36f12dd21193
Britney Khuu <britneykhuu@gmail.com>
2020-11-28 15:00:04 -0800

    began coding for moving platform; added layers and tags for platforms and its colliders

--- DungeonDeliveryService/Assets/Scripts/MovingPlatform.cs
+++ DungeonDeliveryService/Assets/Scripts/MovingPlatform.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public int distance;
+    // axis = x-axis vs y-axis
+    public bool axis;
     public int limLeft;
-    public int limRight;
+    public int movement;
+    public bool leftSide=false;
+    public bool rightSide=false;
 
     // Update is called once per frame
+    //direction false = left or down; direction true = right or up
     void Update()
     {
-        movePlatform();
+        if (leftSide == true)
+        {
+            movePlatformX(false);
+            leftSide = false;
+        }
+        else if (rightSide == true)
+        {
+            movePlatformX(true);
+            rightSide = false;
+        }
+
+        //movePlatform();
     }
-    void movePlatform()
+  //moves item direction based on editor input
+    void movePlatformX(bool direction)
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (direction == false)
+        {
+            //below has a Time.deltaTime, but didnt seem like it was needed so left it be
+            transform.position = transform.position + new Vector3(horizontalInput *movement,verticalInput,0);
+        } 
+        else if (direction == true)
+        {
+            transform.position = transform.position + new Vector3(horizontalInput * -movement, verticalInput, 0);
+        }
+    }
+
+    void movePlatformY(bool direction)
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (direction == false)
+        {
+            //below has a Time.deltaTime, but didnt seem like it was needed so left it be
+            transform.position = transform.position + new Vector3(horizontalInput,verticalInput *movement,0);
+        } 
+        else if (direction == true)
+        {
+            transform.position = transform.position + new Vector3(horizontalInput, verticalInput * -movement, 0);
+        }
     }
     
 }
